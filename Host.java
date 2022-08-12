f08fa4bc8d4fc3bd5af0a53b54c0f008592ddae3
tjdlsqja <109319867+tjdlsqja@users.noreply.github.com>
2022-08-12 14:42:18 +0900

    Add files via upload

---
+++ Host.java
@@ -0,0 +1,22 @@
+package Pos;
+
+import java.util.Scanner;
+
+public interface Host {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	String SSID = "asd"; // 설정된 ID
+	String USERNAME = "서인범"; // 유저 이름
+	int WAGE = 9800; 
+	
+	void changePassword();
+	void login();
+	void logout();
+	void setStartTime(long sTime);
+	void setEndTime(long eTime);
+	double getStartTime();
+	double getEndTime();
+	void calculatePay();
+	double getPay();
+}
